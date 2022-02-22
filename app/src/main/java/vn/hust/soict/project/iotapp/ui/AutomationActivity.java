b090ba5330bf8c944056622fe4ec9e7266687746
yenle <yen.lt121100@gmail.com>
2022-02-23 01:52:07 +0700

    ....

---
+++ app/src/main/java/vn/hust/soict/project/iotapp/ui/AutomationActivity.java
@@ -0,0 +1,16 @@
+package vn.hust.soict.project.iotapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.hust.soict.project.iotapp.R;
+
+public class AutomationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_automation);
+    }
+}
